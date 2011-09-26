9720a320e4dd9099722ac55dbee8572f09479ef5
Denis Zharkov <bintree@sibears.org>
2011-09-26 17:31:09 +0700

    Revert "test commit"
    This reverts commit e980e1805843cecf6502680269201244647d54df.

--- README.txt
+++ README.txt
@@ -2,4 +2,3 @@ Atexant stands for "Academic TEXt ANalizing Tool" developed in
 Computer science department of Tomsk state university and purposed for
 research tasks in area of Data Mining, especially designed for text
 processing in wikimedia formats.
-t
--- java/AtexantApp.java
+++ java/AtexantApp.java
@@ -1,54 +1,9 @@
-import java.util.*;
-import edu.stanford.nlp.pipeline.*;
-import edu.stanford.nlp.util.*;
-import edu.stanford.nlp.ling.CoreAnnotations.*;
-import edu.stanford.nlp.ling.*;
-import edu.stanford.nlp.trees.TreeCoreAnnotations.*;
-import edu.stanford.nlp.trees.*;
-import edu.stanford.nlp.trees.semgraph.*;
-import edu.stanford.nlp.trees.semgraph.SemanticGraphCoreAnnotations.*;
+
 
 public class AtexantApp
 {
     public static void main(String[] args)
     {
 	System.out.println("Just simple application!!!");
-        Properties props = new Properties();
-        props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-    
-        // read some text in the text variable
-        String text = "Stanford University is located in California. It is a great university.";
-
-        // create an empty Annotation just with the given text
-        Annotation document = new Annotation(text);
-
-        // run all Annotators on this text
-        pipeline.annotate(document);
-
-        // these are all the sentences in this document
-        // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
-        List<CoreMap> sentences = document.get(SentencesAnnotation.class);
-
-        for(CoreMap sentence: sentences) {
-          // traversing the words in the current sentence
-          // a CoreLabel is a CoreMap with additional token-specific methods
-          for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
-            // this is the text of the token
-            String word = token.get(TextAnnotation.class);
-            // this is the POS tag of the token
-            String pos = token.get(PartOfSpeechAnnotation.class);
-            // this is the NER label of the token
-            String ne = token.get(NamedEntityTagAnnotation.class); 
-            System.out.println(word + ' ' + pos + ' ' + ne);
-          }
-
-          // this is the parse tree of the current sentence
-          Tree tree = sentence.get(TreeAnnotation.class);
-
-          // this is the Stanford dependency graph of the current sentence
-          SemanticGraph dependencies = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
-        }
-  
     }
 }
